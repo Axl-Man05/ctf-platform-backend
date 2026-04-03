9fee19dea35c2fbc2941703961be68d151cdb40c
AxlMan05 <axelminimi2005@gmail.com>
2026-04-03 15:32:01 -0600

    feat: implementacion de CustomUserDetailsService para conectar el login con MariaDB

---
+++ platform/src/main/java/com/ctf/platform/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.ctf.platform.service;
+
+
+import com.ctf.platform.entity.User;
+import com.ctf.platform.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("el usuario no esta registrado"));
+    }
+}
