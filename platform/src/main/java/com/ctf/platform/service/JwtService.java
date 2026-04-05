29f70133e0b23c75fee1fb8b0bfa3ef93705ab42
AxlMan05 <axelminimi2005@gmail.com>
2026-04-05 14:26:33 -0600

    feat: implementacion de JwtService para la generacion y firmado criptografico de tokens JWT

---
+++ platform/src/main/java/com/ctf/platform/service/JwtService.java
@@ -0,0 +1,34 @@
+package com.ctf.platform.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    private final int jwtExpirationMs = 86400000;
+
+    private Key getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(this.jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateJwtToken(UserDetails userDetails){
+
+        return Jwts.builder()
+                .subject((userDetails.getUsername()))
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(getSigningKey())
+                .compact();
+    }
+}
