656b7b707c6e1888081af75246ec11b1a6a8dd8d
AxlMan05 <axelminimi2005@gmail.com>
2026-04-07 18:54:19 -0600

    refactor: optimizacion de JwtService, correccion de SecretKey y proteccion isTokenValid contra NPE

--- platform/src/main/java/com/ctf/platform/service/JwtService.java
+++ platform/src/main/java/com/ctf/platform/service/JwtService.java
@@ -1,15 +1,18 @@
 package com.ctf.platform.service;
 
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class JwtService {
@@ -17,7 +20,7 @@ public class JwtService {
     private String jwtSecret;
     private final int jwtExpirationMs = 86400000;
 
-    private Key getSigningKey(){
+    private SecretKey getSigningKey(){
         byte[] keyBytes = Decoders.BASE64.decode(this.jwtSecret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
@@ -31,4 +34,31 @@ public class JwtService {
                 .signWith(getSigningKey())
                 .compact();
     }
+
+    public String extractEmail(String token){
+        try{
+            return Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload().getSubject();
+
+        }catch (ExpiredJwtException e){
+            System.out.println("El token ha expirado");
+        }catch (SignatureException e){
+            System.out.println("Firma del token invalida");
+        }catch (MalformedJwtException e){
+           System.out.println("formato del token incorrecto");
+        }catch (JwtException | IllegalArgumentException e){
+            System.out.println("Error al procesar el token: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        String email = extractEmail(token);
+        return userDetails.getUsername().equals(email);
+    }
+
+
 }
