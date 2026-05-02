a570f6fb8a359d0eeb2154f96525e45dbac9f9f5
AxlMan05 <axelminimi2005@gmail.com>
2026-05-02 16:28:03 -0600

    restrict test endopoints to development enviroment

--- platform/src/main/java/com/ctf/platform/controller/TestController.java
+++ platform/src/main/java/com/ctf/platform/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.ctf.platform.controller;
 
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test")
+@Profile("dev")
 public class TestController {
 
     @GetMapping("/ping")
